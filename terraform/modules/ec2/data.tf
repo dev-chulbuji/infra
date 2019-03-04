061b6417ac0fbb02acc519944fa5fbffd226f945
dev-chulbuji <dev.chulbuji@gmail.com>
2019-03-04 17:45:08 +0900

    feat: get subnet what az want

--- terraform/modules/ec2/data.tf
+++ terraform/modules/ec2/data.tf
@@ -8,6 +8,9 @@ data "aws_subnet_ids" "selected" {
   vpc_id = "${data.aws_vpc.selected.id}"
 
   tags {
-    Tier = "${var.subnet_type}"
+    Tier = "${format("%s-%s", var.subnet_type, substr(element(split("-", var.availability_zone), 2), 1, 1))}"
   }
 }
+
+# subnet_type = "public"
+# availability_zone = "ap-northeast-1a"
