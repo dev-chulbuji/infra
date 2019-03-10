f0d1daa7028f4b73d93fc04df3ee54bb05968f51
dev-chulbuji <dev.chulbuji@gmail.com>
2019-03-10 18:00:51 +0900

    feat: change s3 terraform with workspace

--- terraform/s3/s3.tf
+++ terraform/s3/s3.tf
@@ -1,4 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket  = "dj-terraform-backend-dev"
+    key     = "s3bucket/terraform.tfstate"
+    region  = "ap-northeast-1"
+    encrypt = true
+    dynamodb_table = "dj-TerraformStateLock-dev"
+    acl = "bucket-owner-full-control"
+  }
+}
+
 locals {
+  tier = "${terraform.workspace}"
   region = "${terraform.workspace == "dev" ? "ap-northeast-1" : "ap-northeast-2"}"
 }
 
@@ -9,8 +21,8 @@ provider "aws" {
 
 module "s3" {
   source = "../modules/s3"
-  bucket = "dj-s3${terraform.workspace == "dev" ? "-dev" : "-${terraform.workspace}"}"
-  region = "ap-northeast-1"
+  bucket_name = "dj-s3-${local.tier}"
+  region = "${local.region}"
   acl = "public-read"
   versioning_enabled = "true"
 } 
