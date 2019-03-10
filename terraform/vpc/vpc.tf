ba7116f5d8026ae176ab5111dcc6c9f80696e640
dev-chulbuji <dev.chulbuji@gmail.com>
2019-03-10 14:46:05 +0900

    feat: change cidr

--- terraform/vpc/vpc.tf
+++ terraform/vpc/vpc.tf
@@ -28,12 +28,12 @@ module "vpc" {
   source = "../modules/vpc"
 
   name = "chulbuji"
-  cidr = "172.18.0.0/16"
+  cidr = "172.17.0.0/16"
 
   azs              = "${slice(data.aws_availability_zones.available.names, 0, 2)}"
-  public_subnets   = ["172.18.1.0/24", "172.18.2.0/24"]
-  private_subnets  = ["172.18.101.0/24", "172.18.102.0/24"]
-  database_subnets  = ["172.18.201.0/24", "172.18.202.0/24"]
+  public_subnets   = ["172.17.1.0/24", "172.17.2.0/24"]
+  private_subnets  = ["172.17.101.0/24", "172.17.102.0/24"]
+  database_subnets  = ["172.17.201.0/24", "172.17.202.0/24"]
   ingress_cidr_blocks = ["${data.external.myip.ip}"]
   
   tags = {
